c81fc4eb6017c7f23f922dfffdac89353f2cea93
Jazzspencerrosento <Jazzspencerrosento@gmail.com>
2026-03-11 13:33:46 +0800

    update

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from django.contrib import admin
+from studentorg import views
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
