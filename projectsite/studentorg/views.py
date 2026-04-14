be5950a6f921884418aed14b43853e6b175db6e6
jazz <201980319@psu.palawan.edu.ph>
2026-04-14 12:31:05 +0800

    setup static files and homepage

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView    
+from studentorg.models import Organization
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Organization              # ← fetch all records from Organization table
+    context_object_name = 'home'   # ← in the template: {{ home }} = the data
+    template_name = "home.html"     # ← use this HTML file to display the page
+
