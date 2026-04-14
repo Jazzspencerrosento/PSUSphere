9f20322a05a4e86f247bee1d5a0f83fc049d4e19
jazz <201980319@psu.palawan.edu.ph>
2026-04-14 16:50:35 +0800

    update

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -21,5 +21,5 @@ from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'), path('organization_list', OrganizationList.as_view(), name='organization-list'), 
 ]
