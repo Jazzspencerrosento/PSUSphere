d5b0da6af93d6810227e0bf14b20b9a8ec44fd21
jazz <201980319@psu.palawan.edu.ph>
2026-04-15 11:00:36 +0800

    update

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView    
+from django.views.generic.list import ListView  
+from django.views.generic.edit import CreateView, UpdateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization              
@@ -13,3 +16,14 @@ class OrganizationList(ListView):
     template_name = 'org_list.html' 
     paginate_by = 5    
 
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')
+    
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')    
